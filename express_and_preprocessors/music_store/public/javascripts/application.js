8105dfc9a8359ad192de18541c1849cef33b5a52
Daron Spaulding <dspaulding25@gmail.com>
2017-08-14 12:13:03 -0600

    added backbone models/collections/views and handlebars templating

---
+++ express_and_preprocessors/music_store/public/javascripts/application.js
@@ -0,0 +1,15 @@
+var App = {
+  templates: JST,
+  $el: $('main'),
+  renderAlbums: function() {
+    this.albums.each(this.renderAlbumView);
+  },
+  renderAlbumView: function(album) {
+    new AlbumView({
+      model: album
+    })
+  },
+  init: function() {
+    this.renderAlbums();
+  }
+};
